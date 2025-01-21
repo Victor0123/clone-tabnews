ee3f61785f3aa0e46d28a075ebd59a33f58479c5
Victor Rossini <victor.rossini01@hotmail.com>
2025-01-21 17:23:30 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-  return <h1>Obrigado pelos momentos incriveis q passamos juntos, se vc leu me deve uma termal kkkk</h1>
+function Home() {
+  return (
+    <h1>
+      Obrigado pelos momentos incriveis q passamos juntos, se vc leu me deve uma
+      termal kkkk
+    </h1>
+  );
 }
 
 export default Home;
