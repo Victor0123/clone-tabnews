91bc5f19fcd4da770c1d6c1be52910822cd3cf45
Victor Rossini <victor.rossini01@hotmail.com>
2025-04-16 16:33:42 -0300

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,53 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+
+  return responseBody;
+}
+
+export default function StatusPage() {
+  useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  return (
+    <>
+      <h1>Status</h1>
+      <Status />
+    </>
+  );
+}
+
+function Status() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let UpdatedAtText = "Carregando...";
+  let databaseStatus = "Carregando...";
+
+  if (!isLoading && data) {
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    databaseStatus = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>Última atualização: {UpdatedAtText}</div>
+      <h2>Database</h2>
+      <div>{databaseStatus}</div>
+    </>
+  );
+}
